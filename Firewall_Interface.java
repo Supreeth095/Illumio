1e3d2f215ab47995a23ed8efcaf4b40f5369a2f9
Supreeth <supreeth095@gmail.com>
2020-02-07 19:05:58 -0500

    Update Firewall_Interface.java

--- Firewall_Interface.java
+++ Firewall_Interface.java
@@ -1,3 +1,5 @@
+import java.net.UnknownHostException;
+
 public interface Firewall_Interface {
-    boolean accept_packet(String direction, String protocol, int port, String ip_address  );
+    boolean accept_packet(String direction, String protocol, int port, String ip_address ) throws UnknownHostException;
 }
